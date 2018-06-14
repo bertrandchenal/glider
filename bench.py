8f9d3399409161f4ea5f8e028201897e31b61557
Bertrand Chenal <bertrand@adimian.com>
2018-06-14 10:41:09 +0200

    Better bench

--- bench.py
+++ bench.py
@@ -9,29 +9,36 @@ import psutil
 
 current_process = psutil.Process()
 
+def get_data(factor):
+    n = lambda i: 'ham{0} ham{0} foo{0}'.format(i).split()
+    names = lambda s: list(chain.from_iterable(n(i) for i in range(s)))
+    values = [1, 2, 3]
+    # Create two sets of arrays
+    d1 = {'name': names(100 * factor)}
+    d2 = {'name': names(10 * factor)}
+    # Add some columns
+    new_cols =  [
+        (100 * factor, d1, 'abc'),
+        (10 * factor, d2, 'fgh')]
+    for size, d, names in new_cols:
+        for n in names:
+            d[n] = values * size
+    return d1, d2
 
 def get_mem():
     mem = current_process.memory_info()
     return mem.rss
 
-def join_bench():
+def join_bench(factor):
     print('-- join --')
-    n = lambda i: 'ham{0} ham{0} foo{0}'.format(i).split()
-    names = lambda s: list(chain.from_iterable(n(i) for i in range(s)))
-    values = [1, 2, 3]
-    # Create two sets of arrays
-    d1 = {'name': names(1000)}
-    d2 = {'name': names(100)}
-    # Add some columns
-    for factor, d in [(1000, d1), (100, d2)]:
-        for col in 'abcde':
-            d[col] = values * factor
+    d1, d2 = get_data(factor)
 
     start = time()
     f1 = Frame(d1)
     f2 = Frame(d2)
     mem = get_mem()
     f3 = f1.join(f2, 'name', how='inner')
+    # print(f3['e'].sum() / factor)
     print('glider:', time() - start, get_mem() - mem)
     del f1
     del f2
@@ -42,6 +49,8 @@ def join_bench():
     f2 = DataFrame(d2)
     mem = get_mem()
     f3 = f1.merge(f2, on='name')
+
+    # print(f3['e'].sum() / factor)
     print('pandas:', time() - start, get_mem() - mem)
     # Output
     # glider: 0.0130 385024
@@ -79,5 +88,6 @@ def groupby_bench():
 
 
 if __name__ == '__main__':
-    join_bench()
+    for factor in range(10, 100, 10):
+        join_bench(factor)
     groupby_bench()
